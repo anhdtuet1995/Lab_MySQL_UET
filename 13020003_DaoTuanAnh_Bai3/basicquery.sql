4146106c891c23503a7b11ee953fc37d7eef8e9e
anhdt_58 <anhdt_58@vnu.edu.vn>
2015-10-06 01:03:31 +0700

    bt3

---
+++ 13020003_DaoTuanAnh_Bai3/basicquery.sql
@@ -0,0 +1,29 @@
+/*
+* In ra danh sach nhan vien co truong reportsTo chua xac dinh
+*/
+
+SELECT * FROM employees WHERE reportsTo is NULL;
+
+/*
+* In ra cac CustomerNumber co thuc hien giao dich
+*/
+
+SELECT customerNumber FROM customers WHERE salesRepEmployeeNumber is NOT NULL;
+
+/*
+* Dua ra danh sach cac don hang co ngay yeu cau van chuyen la 18/1/2003
+*/
+
+SELECT orderNumber FROM orders WHERE requiredDate = "2003/1/18";
+
+/**
+* Dua ra cac don hang co ngay dat trong thang 4/2005 va cos trang thai la shipped
+*/
+
+SELECT orderNumber FROM orders WHERE YEAR(orderDate) = 2003 AND MONTH(orderDate) = 4 AND status = "Shipped";
+
+/**
+ * Dua ra cac san pham thuoc nhom Classic Cars
+ */
+
+SELECT productName FROM products WHERE productLine = "Classic Cars";
