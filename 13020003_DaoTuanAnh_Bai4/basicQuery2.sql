86ad9e29281f369ea5417c387d8c8e2149e5afe9
anhdt_58 <anhdt_58@vnu.edu.vn>
2015-10-06 01:39:16 +0700

    bt4

---
+++ 13020003_DaoTuanAnh_Bai4/basicQuery2.sql
@@ -0,0 +1,40 @@
+/**
+ *
+ * In ra danh sach khach hang o Nantes hoac Lyon
+ *
+ */
+
+SELECT * FROM customers WHERE city IN ("Nantes", "Lyon");
+
+/**
+ *
+ * In ra don hang co thoi gian chuyen tu 10/1/2003 den 10/3/2003
+ *
+ */
+
+SELECT * FROM orders WHERE shippedDate BETWEEN "2003/1/10" AND "2003/3/10";
+
+/**
+ *
+ * danh sach san pham thoai loai san pham co chua chu "CARS"
+ *
+ */
+
+SELECT productName FROM products WHERE productLine LIKE "%CARS%";
+
+/**
+ *
+ * in ra 10 san pham co so luong hang ton nhieu nhat
+ *
+ */
+
+SELECT productName, quantityInStock FROM products ORDER BY quantityInStock DESC LIMIT 10;
+
+/**
+ *
+ * in ra danh sach san pham va them 1 cot la tong gia tri hang ton
+ *
+ */
+
+SELECT productName, (quantityInStock * buyPrice) AS priceQuantityInStock FROM products;
+  	
