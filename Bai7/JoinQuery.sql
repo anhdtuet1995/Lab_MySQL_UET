4d1405ac440b4ad736ea64bd402e72cadf0611c6
anhdt_58 <anhdt_58@vnu.edu.vn>
2015-10-27 09:42:23 +0700

    bai7 1

---
+++ Bai7/JoinQuery.sql
@@ -0,0 +1,21 @@
+USE classicmodels;
+
+/*
+Ten nhan vien + ten cong ty
+*/
+SELECT 	
+	CONCAT(e.firstName, ' ', e.lastName) as fullName,
+	o.territory
+		FROM employees e
+			INNER JOIN offices o ON e.officeCode = o.officeCode;
+
+
+/*
+In ra ten khach hang va cac san pham
+*/	
+
+SELECT 
+	c.customerName
+		FROM customers c
+			INNER JOIN orders o ON c.customerNumber = o.customerNumber
+			INNER JOIN orderdetails od ON o.orderNumber = od.orderNumber;
